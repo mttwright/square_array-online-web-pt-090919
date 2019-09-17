c65bd2e72e152d9affefeca7252d3173e7c0fa20
virtual-frame-7789 <mttwright@gmail.com>
2019-09-17 19:17:52 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
+#def square_array(array)
+#  new_array = []
+#  counter = 0
+#    array.each do |number|
+#      new_array[counter] = number * number
+#      counter += 1
+#    end
+#  new_array
+#end
+
 def square_array(array)
-  # your code here
-end
