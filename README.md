f612c0f2e123c3f860b10d5d3594d7d0992edcd4
virtual-frame-7789 <mttwright@gmail.com>
2019-09-17 19:27:03 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 # Square Array
 
 ## Objectives
--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  counter = 0
+    array.each do |number|
+      new_array[counter] = number * number
+      counter += 1
+    end
+  new_array
 end
+
+#def square_array(array)
+#  array.collect do |number|
+#    number * number
+#  end
+#end
+    
